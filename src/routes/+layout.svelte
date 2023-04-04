a498ad920bb033c3397c436a3aeaa2cfa05ae72d
Skylan Lew <skylanslew@gmail.com>
2023-04-04 00:33:55 -0700

    init page and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { IconMenu2 } from '@tabler/icons-svelte'
+	import { fly } from 'svelte/transition';
+	import Sidebar from '../components/nav/Sidebar.svelte'
+
+	let sidebar_show = false;
+
+</script>
+
+<style>
+	main {
+		/* background-color: blue; */
+		height: 90%;
+	}
+
+	.wrapper {
+		margin: 0;
+		overflow-x: hidden;
+		/* background-color: orangered; */
+		min-width: 100vw;
+		height: 100vh;
+	}
+
+	.menu-button{
+		background: none;
+		border: none;
+	}
+
+	header{
+		display: flex;
+		justify-content: space-between;
+		height: 5%;
+	}
+
+	footer{
+		height: 5%;
+	}
+
+</style>
+<div class="wrapper">
+	<header>
+		bgtools
+		<button class="menu-button" on:click={() => {sidebar_show = !sidebar_show}}>
+			<IconMenu2 />
+		</button>
+	</header>
+
+		<main>
+			<slot />
+			<Sidebar bind:show={sidebar_show} />
+		</main>
+
+	<footer>
+		by compass
+	</footer>
+</div>
+
